220df54b6dfbc2da3be3e10cc000e3aad6a49cfb
Yhonny <ramirezyhonny0@gmail.com>
2024-09-07 22:15:21 -0300

    update routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,17 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
-import { routeTree } from "./routeTree.gen";
-import { StrictMode } from "react";
-import theme from "./theme";
 
-const router = createRouter({ routeTree });
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import {routeTree} from './routeTree.gen'
 
-// Render the app
-const rootElement = document.getElementById("root");
+const router = createRouter({ routeTree })
 
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <ChakraProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </StrictMode>
-  );
+const rootElement = document.getElementById('root');
+if (rootElement && !rootElement.innerHTML){
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router}/>
+        </StrictMode>,
+    )
 }
